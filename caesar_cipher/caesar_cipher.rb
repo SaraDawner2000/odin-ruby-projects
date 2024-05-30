11b4aaeea633fc61d0a95ae628ed01bd0d85922c
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-05-30 16:53:04 -0500

    correct code style with RuboCop

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 def caesar_cipher(string, shift)
-    result = []
-    string.chars.each do |letter|
-        letter_num = letter.ord
-        case letter_num
-        when "A".ord.."Z".ord
-            letter_num + shift <= "Z".ord ? result.push((letter_num + shift).chr) : result.push((letter_num + shift - 26).chr)
-        when "a".ord..'z'.ord
-            letter_num + shift <= "z".ord ? result.push((letter_num + shift).chr) : result.push((letter_num + shift - 26).chr)
-        else
-            result.push(letter_num.chr)
-        end
+  result = []
+  string.chars.each do |letter|
+    letter_num = letter.ord
+    case letter_num
+    when ('A'.ord)..('Z'.ord)
+      letter_num + shift <= 'Z'.ord ? result.push((letter_num + shift).chr) : result.push((letter_num + shift - 26).chr)
+    when ('a'.ord)..('z'.ord)
+      letter_num + shift <= 'z'.ord ? result.push((letter_num + shift).chr) : result.push((letter_num + shift - 26).chr)
+    else
+      result.push(letter_num.chr)
     end
-    result.join("")
+  end
+  result.join
 end
-puts caesar_cipher("WHAT A STRING!", 5)
+puts caesar_cipher('WHAT A STRING!', 5)
